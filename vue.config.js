ebe75176c11889368fe74be719d894fa8fe34ade
spacewave1 <w.nabok@gmail.com>
2020-09-23 02:29:46 +0200

    Adjust config

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,1 @@
-module.exports = {    publicPath: process.env.NODE_ENV === 'production'    ? '/hello-world/'    : '/'}
+module.exports = {    publicPath: process.env.NODE_ENV === 'production'    ? '.'    : '/'}
